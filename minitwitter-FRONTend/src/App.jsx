ac3cacfad42ee0e2528631b68df0e9e861ed34f0
Paula Fernández <better_days1996@hotmail.com>
2024-10-02 21:43:46 +0200

    Corrección de errores y actualización para el despliegue

--- minitwitter-FRONTend/src/App.jsx
+++ minitwitter-FRONTend/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./routes/Layout.jsx";
 import { Home } from "./routes/Home.jsx";
+import { Users } from "./routes/Users.jsx";
+import { Tweets } from "./routes/Tweets.jsx";
 
 function App() {
   return (
@@ -9,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="users" element={"dwcwdc"} />
-          <Route path="tweets" element={"dcdwc"} />
+          <Route path="users" element={<Users />} />
+          <Route path="tweets" element={<Tweets />} />
         </Route>
       </Routes>
     </>
